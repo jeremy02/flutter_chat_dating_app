717c40162b1dd6a349f7dc5cbe5f10bcd88ffbd1
Jeremiah Thuku <jeremiah.thuku@jubileekenya.com>
2019-10-14 12:48:31 +0300

    added list animation

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_chat_dating_app/components/message_list.dart';
 import 'package:flutter_chat_dating_app/models/chat_model.dart';
-import 'package:flutter_chat_dating_app/widgets/custom_border.dart';
-import 'package:intl/intl.dart';
 
 class MessageScreen extends StatefulWidget {
 	
@@ -34,23 +33,13 @@ class MessageItem implements ListItem {
 
 class MessageScreenState extends State<MessageScreen> {
 	
+	GlobalKey<AnimatedListState> listKey = GlobalKey();
+	
 	final messageController = TextEditingController();
 	
 	bool isMessageInputEdited = false;
 	
 	ScrollController chatMessagesSrollController = new ScrollController();
-	
-	// demo chat messages list
-//	List<String> lMessageItems = [
-//		"what really happens when they become chosen to leave the grocery store.",
-//		"Some trial work for the client.",
-//		"is about one sausage leading a group of supermarket products on a quest to discover the truth about their existence",
-//		"Vote for your favorite community uploads created for our latest challenge",
-//		"Sarah",
-//		"Sausage Party, the first R-rated CG animated movie",
-//		"Filter quality downloads for your next project by softwares",
-//		"This is an Education learning app with a gamification concept. Easy to learn and fun to learn."];
-	
 	
 	@override
 	void initState() {
@@ -126,7 +115,27 @@ class MessageScreenState extends State<MessageScreen> {
 					Column(
 						mainAxisSize: MainAxisSize.max,
 						children: <Widget>[
-							messageListComponent(context),
+							Expanded(
+								flex: 1,
+								child: Container(
+									padding: EdgeInsets.only(bottom: 20.0),
+									decoration: BoxDecoration(
+										color: Colors.white,
+										borderRadius: BorderRadius.only(
+											bottomLeft: Radius.circular(36.0),
+											bottomRight: Radius.circular(36.0),
+										),
+									),
+									child: lMessageItems.length <= 0 ?
+									null
+										:
+									MessagesList(
+										srollController: chatMessagesSrollController,
+										lMessageChatItems : lMessageItems,
+										listKey : listKey,
+									),
+								),
+							),
 							createMessageInputComponent(context),
 						],
 					),
@@ -151,225 +160,6 @@ class MessageScreenState extends State<MessageScreen> {
 					)
 				],
 			),
-		);
-	}
-	
-	Widget messageListComponent (BuildContext context) {
-		// sort the list using dates
-		lMessageItems.sort((a, b) => a.date.compareTo(b.date));
-		
-		// model to compare to
-		ChatModel prev = lMessageItems[0];
-		bool shownHeader = false;
-		
-		List<ListItem> _listChildren = <ListItem>[];
-		lMessageItems.forEach((ChatModel model) {
-			
-			if (prev != null && DateTime(model.date.year, model.date.month, model.date.day) != DateTime(prev.date.year, prev.date.month, prev.date.day)) {
-				shownHeader = false; // if dates are different, beginning of a new group, so header is yet to be shown
-			}
-			
-			if (!shownHeader) {
-				// if header for a group is not shown yet, add it to the list
-				_listChildren.add(HeadingItem(model));
-				prev = model; // keep the current model for reference to check if group has changed
-				shownHeader = true;
-			}
-			
-			_listChildren.add(MessageItem(model));
-			
-		});
-		
-		_listChildren = _listChildren.reversed.toList();
-		
-		return Expanded(
-			flex: 1,
-			child: Container(
-				padding: EdgeInsets.only(bottom: 20.0),
-				decoration: BoxDecoration(
-					color: Colors.white,
-					borderRadius: BorderRadius.only(
-						bottomLeft: Radius.circular(36.0),
-						bottomRight: Radius.circular(36.0),
-					),
-				),
-				child: _listChildren.length <= 0 ?
-					null
-					:
-					ListView.builder(
-						itemCount: _listChildren.length,
-						reverse: true,
-						physics: BouncingScrollPhysics(),
-						shrinkWrap: true,
-						controller: chatMessagesSrollController,
-						itemBuilder: (context, index) {
-							final item = _listChildren[index];
-							
-							
-							if (item is HeadingItem) {
-								
-								final textDate = item.heading.date;
-								final dateNow = DateTime.now();
-								final difference = dateNow.difference(textDate).inDays;
-								
-								return Row(
-									mainAxisSize: MainAxisSize.max,
-									crossAxisAlignment: CrossAxisAlignment.center,
-									mainAxisAlignment: MainAxisAlignment.center,
-									children: <Widget>[
-										Container(
-											padding: EdgeInsets.symmetric(horizontal: 10.0),
-											margin: EdgeInsets.symmetric(vertical: 10.0),
-											decoration: BoxDecoration(
-												borderRadius: BorderRadius.only(
-													bottomLeft: Radius.circular(16.0),
-													bottomRight: Radius.circular(16.0),
-												),
-												color: Colors.white,
-												boxShadow: <BoxShadow>[
-													CustomBoxShadow(
-														color: Colors.grey.shade300,
-														offset: new Offset(0.0, 5.0),
-														blurRadius: 5.0,
-														blurStyle: BlurStyle.outer,
-														spreadRadius: 1.0,
-													)
-												],
-											),
-											child: Center(
-												child: Text(
-													difference == 1 ? "Yesterday"
-														:
-													(difference == 0 ? "Today" : new DateFormat.MMMd().format(item.heading.date))
-													,
-													style: TextStyle(
-														color: Colors.red.shade900,
-														fontSize: 14.0,
-													),
-												),
-											),
-										),
-									],
-								);
-							}else if (item is MessageItem) {
-								return messageItemComponent(item.content, context, index.isOdd,index);
-							}else{
-								return null;
-							}
-						},
-					),
-			),
-		);
-	}
-	
-	Widget messageItemComponent(ChatModel lMessageItem, context, bool isOdd, int index) {
-		
-		final mWidth = MediaQuery.of(context).size.width * 0.75;
-		
-		double marginTop = index == 1 ? 10 : 20;
-		
-		return Row(
-			mainAxisSize: MainAxisSize.max,
-			mainAxisAlignment: isOdd ? MainAxisAlignment.start : MainAxisAlignment.end,
-			children: <Widget>[
-				Container(
-					constraints: BoxConstraints(
-						maxWidth: mWidth,
-					),
-					margin: EdgeInsets.fromLTRB(20.0, marginTop, 20.0, 20.0),
-					decoration: BoxDecoration(
-						color: Colors.transparent,
-						borderRadius: BorderRadius.circular(10),
-					),
-					child: isOdd ?
-						receivedMessageLayout(lMessageItem)
-						:
-						sentSentMessageLayout(lMessageItem)
-					,
-				),
-			],
-		);
-	}
-	
-	Widget receivedMessageLayout(ChatModel lMessageItem) {
-		return Row(
-			crossAxisAlignment: CrossAxisAlignment.start,
-			children: <Widget>[
-				CircleAvatar(
-					radius: 18.0,
-					backgroundImage:NetworkImage(
-						'https://via.placeholder.com/150',
-					),
-					backgroundColor: Colors.transparent,
-				),
-				SizedBox(width: 8.0,),
-				Flexible(
-					child: Column(
-						crossAxisAlignment: CrossAxisAlignment.start,
-						children: <Widget>[
-							Text(
-								lMessageItem.text,
-								style: TextStyle(
-									color: Colors.black,
-									fontWeight: FontWeight.bold,
-								),
-							),
-							SizedBox(height: 4.0,),
-							Text(
-								new DateFormat.jm().format(lMessageItem.date).toString(),
-								style: TextStyle(
-									fontSize: 12.0,
-									color: Colors.grey.shade400,
-									fontWeight: FontWeight.bold,
-								),
-							),
-						],
-					),
-				),
-			],
-		);
-	}
-	
-	Widget sentSentMessageLayout(ChatModel lMessageItem) {
-		return Column(
-			crossAxisAlignment: CrossAxisAlignment.end,
-			children: <Widget>[
-				Container(
-					padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-					decoration: BoxDecoration(
-						gradient: LinearGradient(
-							stops: [0.4, 1.0],
-							begin: FractionalOffset.topCenter,
-							end: FractionalOffset.bottomCenter,
-							colors: [
-								Color.fromRGBO(238,101,95, 1.0),
-								Color.fromRGBO(237,74,91, 1.0),
-							],
-						),
-						borderRadius: BorderRadius.only(
-							topRight: Radius.circular(24.0),
-							topLeft: Radius.circular(24.0),
-							bottomLeft: Radius.circular(24.0),
-						),
-					),
-					child: Text(
-						lMessageItem.text,
-						style: TextStyle(
-							color: Colors.white,
-							fontWeight: FontWeight.normal,
-						),
-					),
-				),
-				SizedBox(height: 2.0,),
-				Text(
-					new DateFormat.jm().format(lMessageItem.date).toString(),
-					style: TextStyle(
-						fontSize: 12.0,
-						color: Colors.grey.shade400,
-						fontWeight: FontWeight.bold,
-					),
-				),
-			],
 		);
 	}
 	
@@ -411,6 +201,7 @@ class MessageScreenState extends State<MessageScreen> {
 										date: DateTime.now(),
 										text: message,
 									));
+									listKey.currentState.insertItem(0);
 								});
 							messageController.clear();
 							
